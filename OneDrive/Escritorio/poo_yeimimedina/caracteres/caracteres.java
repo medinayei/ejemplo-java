0b90a7b0860a713d36b06ea0274849e30ea054db
medinayei <20243rd041@utez.edu.mx>
2025-05-06 09:31:52 -0600

    caracteres

---
+++ OneDrive/Escritorio/poo_yeimimedina/caracteres/caracteres.java
@@ -0,0 +1,12 @@
+pubic class Caracteres {
+    public static void main ( String [] args ){
+             char letra = 'J';
+             char simbolo = '@';
+             char numero = '5';
+
+             System.out.println("letra = " + letra );
+              System.out.println("simbolo = " + simbolo );
+               System.out.println("numero = " + numero );
+
+    }
+}
