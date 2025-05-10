444512d92bcb5f8411210c111fa2370e4215605e
medinayeimi <20243rd041@utez.edu.mx>
2025-05-09 22:05:05 -0600

    Iniciando a pedir los numeros

---
+++ OneDrive/Escritorio/poo_yeimimedina/calculadora/calculadora.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane; // libreria para que podamos utilizar un pequenio cuadro de texto 
+public class Calculadora {
+    public static void main ( String args []){
+ String numero1 = JOptionPane.showInputDialog("Ingresa el primer numero ");
+    String numero2 = JOptionPane.showInputDialog("Ingresa el segundo numero ");
+
+    double numeroDecimal1 = Double.parseDouble(numero1);
+    double numeroDecimal2 = Double.parseDouble(numero2);
+    double totalSuma = numeroDecimal1 + numeroDecimal2 ;
+    double totalResta= numeroDecimal1 - numeroDecimal2 ;
+     double totalMultiplicacion = numeroDecimal1 * numeroDecimal2 ;
+      double totalDivision= numeroDecimal1 / numeroDecimal2 ;
+      System.out.println ("El resultado de sumar los 2 numeros que ingresaste es " + totalSuma);
+    }
+}
